754edb7d9262116a8c3ef20d692308f579fde018
Christian Schober <christian@e-man.co.uk>
2018-07-23 21:42:22 +0100

    New Feature Layer

---
+++ TestApp/Utilities/NewFeatureLayer.swift
@@ -0,0 +1,17 @@
+//
+//  NewFeatureLayer.swift
+//  TestApp
+//
+//  Created by Chris on 23/07/2018.
+//  Copyright © 2018 Chris. All rights reserved.
+//
+
+import Foundation
+
+class NewFeatureLayer {
+    
+    func foo() {
+        
+    }
+    
+}

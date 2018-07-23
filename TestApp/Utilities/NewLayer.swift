d368d0c8353310dc77492d2cff031e20d178d0bf
Christian Schober <christian@e-man.co.uk>
2018-07-23 22:38:17 +0100

    first feature

---
+++ TestApp/Utilities/NewLayer.swift
@@ -0,0 +1,17 @@
+//
+//  NewLayer.swift
+//  TestApp
+//
+//  Created by Chris on 23/07/2018.
+//  Copyright © 2018 Chris. All rights reserved.
+//
+
+import Foundation
+
+class NewLayer {
+    
+    func foo() {
+        
+    }
+    
+}
